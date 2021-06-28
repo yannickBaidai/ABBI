a9af43a93e83d6db11af287f82d7d2b70c39122c
yannickBaidai <49522973+yannickBaidai@users.noreply.github.com>
2021-06-28 20:23:06 +0000

    Update 3.1-Check_data_availability.R

--- 1-Routines/3-Analysis_Routines/3.1-Check_data_availability.R
+++ 1-Routines/3-Analysis_Routines/3.1-Check_data_availability.R
@@ -19,19 +19,19 @@ if(is.numeric(SPATIAL_SCALE))
   catch <- unique(read.csv2(file = file.path(m_OUTPUTS, "aggregated_catchdata.csv"))%>%
                     dplyr::filter(species==SPECIES, year %in% YEARS)%>%
                     dplyr::select(year, timescale, zone, x,y))
-  catch$catch <- T
+  catch$m <- T
   
   ### Loading FAD data
   fads <- unique(read.csv2(file = file.path(f1_OUTPUTS, "aggregated_fadData.csv"))%>%
                    dplyr::filter(year %in% YEARS)%>%
                    dplyr::select(year, timescale, zone, x,y))
-  fads$fads <- T
+  fads$f <- T
   
   ### Loading buoy data  
   buoys <- unique(read.csv2(file=file.path(Nfob_OUTPUTS, "aggregated_dailyBuoyDensityData.csv"))%>%
                     dplyr::filter(year %in% YEARS, x >20)%>%
                     dplyr::select(year, timescale, zone, x,y))
-  buoys$buoys <- T
+  buoys$p <- T
   
   ### Merging data source (m, f and p)
   df <- merge.data.frame(x=fads, y = catch, all =T)
@@ -49,28 +49,12 @@ if(is.numeric(SPATIAL_SCALE))
   {
     library(ggplot2)
     world <- map_data("world")
-    ggplot()+
-      geom_tile(data=df, aes(x, y, fill=avail2), color="transparent", size=0.1)+
-      geom_map(data=world, map=world,aes(map_id =region),fill = "dimgray", color="ivory", size=0.3)+
-      labs(x="Longitude", y="Latitude", fill="Data availability")+
-      scale_fill_manual(values=c( "darkgreen", "white"))+
-      coord_quickmap(xlim = c(20,80), ylim=c(-50,50))+
-      theme_linedraw(base_size = 9)+
-      theme(panel.background = element_rect(color="black", fill="lightblue"),
-            panel.grid.major  = element_line(colour = "dimgray", linetype = "dashed", size=0.4),
-            panel.grid.minor  = element_blank(),
-            axis.ticks = element_line(size=1, colour="black"),
-            legend.position = "bottom")+
-      facet_grid(timescale~year)
-    ggsave(filename = file.path(OUTPUTS_DIR, paste0("0.2-data_availability_at_buoysCountThreshold_of", BUOYS_COUNT_THRESHOLD, ".png")), 
-           width = 16, height = 9, dpi = 600)
-    
-    
+   
     # Scatterpie
     df[is.na(df)] <- 0
     ggplot()+
       geom_map(data=world, map=world,aes(map_id =region),fill = "dimgray", color="ivory", size=0.3)+
-      scatterpie::geom_scatterpie(data=df, aes(x, y), cols=c("fads", "catch", "buoys"), color=NA)+
+      scatterpie::geom_scatterpie(data=df, aes(x, y), cols=c("fads", "catch", "buoys"), color=NA, size=6)+
       labs(x="Longitude", y="Latitude", fill="Data availability")+
       coord_fixed(xlim = c(20,80), ylim=c(-50,50))+
       theme_linedraw(base_size = 12)+
